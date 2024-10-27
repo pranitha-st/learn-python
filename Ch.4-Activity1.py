29475af522233928d7c51789ebc9e11e314dbdcf
Pranitha Saravana Thiagarajan <pranitha.sth@gmail.com>
2024-10-27 11:09:36 -0400

    this code prints some of my favorite things from a list using f-string

---
+++ Ch.4-Activity1.py
@@ -0,0 +1,4 @@
+#This code prints some of my favorite things from a list using f-string
+MyName = "Pranitha"
+MyFavoriteThings = ["Madurai", "Biriyani and Kadai Chicken", 6, "Crocheting", "Drawing", "Painting", "Vacation"]
+print(f"These are some of {MyName}'s favorite things: {MyFavoriteThings} ")
