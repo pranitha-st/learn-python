049e85360d2678c6d2ee498cc5d871c395206324
Pranitha Saravana Thiagarajan <pranitha.sth@gmail.com>
2024-10-27 11:26:17 -0400

    This code checks if the cloud shapes I saw and the cloud shapes my friend saw were the same shape using list indices and if statements.

---
+++ Ch.4-Activity2.py
@@ -0,0 +1,27 @@
+#This code checks if the cloud shapes I saw and the cloud shapes my friend saw were the same shape using list indices and if statements.
+MyCloudShapes = ["Circle", "Turtle", "Dolphin", "Truck", "Apple", "Spoon"]
+FriendCloudShapes = ["Apple", "Turtle", "Spoon", "Truck", "Circle", "Dolphin"]
+if MyCloudShapes[0] == FriendCloudShapes[0]:
+    print("Yay, we got the same shape!")
+else:
+    print("Huh, we got different shapes that time.")
+if MyCloudShapes[1] == FriendCloudShapes[1]:
+    print("Yay, we got the same shape!")
+else:
+    print("Huh, we got different shapes that time.")
+if MyCloudShapes[2] == FriendCloudShapes[2]:
+    print("Yay, we got the same shape!")
+else:
+    print("Huh, we got different shapes that time.")
+if MyCloudShapes[3] == FriendCloudShapes[3]:
+    print("Yay, we got the same shape!")
+else:
+    print("Huh, we got different shapes that time.")
+if MyCloudShapes[4] == FriendCloudShapes[4]:
+        print("Yay, we got the same shape!")
+else:
+    print("Huh, we got different shapes that time.")
+if MyCloudShapes[5] == FriendCloudShapes[5]:
+    print("Yay, we got the same shape!")
+else:
+    print("Huh, we got different shapes that time.")
