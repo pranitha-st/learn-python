f9073b006ba8f194f487495797565c816eff42e6
Pranitha Saravana Thiagarajan <pranitha.sth@gmail.com>
2024-10-31 13:08:01 -0400

    This code prints the items needed for each senario from a list in the correct order using its index

---
+++ Ch.4-Activity3.py
@@ -0,0 +1,14 @@
+#This code prints the items needed for each senario from a list in the correct order using its intex.
+RandomItems = ["basket", "tennis", "bread", "table", "ball", "game", "box"]
+#Scenario 1
+print(f"He needs a {RandomItems[1]} {RandomItems[4]} too.")
+#Scenario 2
+print(f"He can take his bread in a {RandomItems[2]} {RandomItems[0]}.")
+#Scenario 3
+print(f"Take me out to the base{RandomItems[4]} {RandomItems[5]}!")
+#Scenario 4
+print(f"She's writing a story about {RandomItems[0]} {RandomItems[4]}.")
+#Scenario 5
+print(f"She put the {RandomItems[2]} in a {RandomItems[2]} {RandomItems[6]}.")
+#Scenario 6
+print(f"He can store it in a board{RandomItems[5]} {RandomItems[6]}.")
