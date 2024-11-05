643d7401367bc332a5d6e68de56c307a124c3fe8
Pranitha Saravana Thiagarajan <pranitha.sth@gmail.com>
2024-11-04 19:25:08 -0500

    This code prints my ages at different years in intervals of 5 years.

---
+++ Ch.4-Activity5.py
@@ -0,0 +1,19 @@
+#This code prints my ages at different years in intervals of 5 years.
+Year = 2024
+Age = 13
+FavoriteOutfit = "t-shirt and jeans"
+FavoriteHobby = "crocheting"
+Age29 = 18
+Age34 = 23
+Age39 = 28
+Age44 = 33
+if Year == 2024:
+    print(f"It's year 2024! I'm currently {Age} years old, love wearing a {FavoriteOutfit}, and currently, {FavoriteHobby} takes up all my time!")
+elif Year == 2029:
+    print(f"It's year 2029! I'm currently {Age29} years old!")
+elif Year == 2034:
+    print(f"It's year 2034! I'm currently {Age34} years old!")
+elif Year == 2039:
+    print(f"It's year 2039! I'm currently {Age39} years old!")
+elif Year == 2044:
+    print(f"It's year 20441 I'm currently {Age44} years old!")
