6a73ed4f5981f7d9efe29a342e422ce68aaface9
Pranitha Saravana Thiagarajan <pranitha.sth@gmail.com>
2024-11-04 19:22:20 -0500

    This code prints the Pet Parade in order, making some changes to te list.

---
+++ Ch.4-Activity4.py
@@ -0,0 +1,8 @@
+#This code prints the Pet Parade in order, making some changes to te list.
+PetParadeOrder = ["Pete the Pug", "Sally the Siamese Cat", "Beau the Boxer", "Lulu the Labrador", "Lily the Lynx", "Pauline the Parrot", "Gina the Gerbil", "Tubby the Tabby Cat"]
+PetParadeOrder.remove("Gina the Gerbil")
+PetParadeOrder = ["Pauline the Parrot", "Pete the Pug", "Sally the Siamese Cat", "Beau the Boxer", "Lulu the Labrador", "Lily the Lynx", "Tubby the Tabby Cat"]
+PetParadeOrder = ["Pauline the Parrot", "Pete the Pug", "Sally the Siamese Cat", "Beau the Boxer", "Lulu the Labrador", "Lily the Lynx", "Mimi the Maltese Cat", "Cory the Corgi", "Tubby the Tabby Cat"]
+PetParadeOrder.remove("Lulu the Labrador")
+PetParadeOrder.remove("Lily the Lynx")
+print(PetParadeOrder)
