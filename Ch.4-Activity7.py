d73fd7c3494de489dad0172f712b1032de6dace4
Pranitha Saravana Thiagarajan <pranitha.sth@gmail.com>
2024-11-04 19:29:51 -0500

    This code is a variety of variables either stored in a list or a tuple

---
+++ Ch.4-Activity7.py
@@ -0,0 +1,4 @@
+#This code is a variety of variables either stored in a list or a tuple
+Collection1 = ("Pranitha", "Saravana Thiagarajan", "Brown", "Black", 10, 10)
+Collection2 = ["Tigers", "Dogs", "Orangutans"]
+Collection3 = ("Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Purple")
